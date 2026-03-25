4a3ea90a3453efc7875351c5bcfbf514af0591a8
Sandra Rivas <srivas18.slp@gmail.com>
2026-03-25 00:39:22 -0600

    Add files via upload
    version inicial

---
+++ Perfil.js
@@ -0,0 +1,17 @@
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "nombre": "Sandra Rivas",
+        "titulo_profesional": "Ingeniería Geomática",
+        "residencia": "San Luis Potosí"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-100.9674, 22.1622]
+      }
+    }
+  ]
+}
---
+++ Script.js
@@ -0,0 +1,66 @@
+// MAPA BASE
+var osm = new ol.layer.Tile({
+  source: new ol.source.OSM()
+});
+
+var estiloPunto = new ol.style.Style({
+  image: new ol.style.Circle({
+    radius: 8,
+    fill: new ol.style.Fill({color: 'red'}),
+    stroke: new ol.style.Stroke({
+      color: 'white',
+      width: 2
+    })
+  })
+});
+
+// CAPA GEOSERVER
+var capaCV = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    format: new ol.format.GeoJSON({
+      dataProjection: 'EPSG:4326',
+      featureProjection: 'EPSG:3857'
+    }),
+    url: function(extent) {
+      return 'http://localhost:8081/geoserver/ows?' +
+        'service=WFS&version=1.0.0&request=GetFeature&typeName=sandy_cvstorymap:perfil_personal&outputFormat=application/json';
+    },
+    strategy: ol.loadingstrategy.all
+  }),
+  style: estiloPunto
+});
+
+// MAPA
+var map = new ol.Map({
+  target: 'map',
+  layers: [osm, capaCV],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([-100.96, 22.16]),
+    zoom: 10
+  })
+});
+
+// CLICK
+map.on('singleclick', function(evt) {
+  map.forEachFeatureAtPixel(evt.pixel, function(feature) {
+    var props = feature.getProperties();
+    alert(JSON.stringify(props, null, 2));
+  });
+});
+
+// SIDEBAR
+const btn = document.getElementById("toggleBtn");
+const sidebar = document.getElementById("sidebar");
+
+btn.onclick = () => {
+  sidebar.classList.toggle("active");
+};
+
+// MOVER MAPA
+function goToLocation(lon, lat) {
+  map.getView().animate({
+    center: ol.proj.fromLonLat([lon, lat]),
+    zoom: 12,
+    duration: 1000
+  });
+}
